c488914b85e074f0d460376a2874962f66ef0fe1
The1Rogue <fabiantschut@gmail.com>
2025-10-16 22:28:29 +0200

    fixed bugs, tried running on more things

--- 5-CST-URS.py
+++ 5-CST-URS.py
@@ -1,79 +1,95 @@
 
 
 from random import randrange
-
-with open("tmp.dddmp") as file:
-	raw = file.read().split("\n")
-
-nodes = raw[11:-2]
-
-levelCount = int(raw[5].split(" ")[1])
-
-varCount = int(raw[4].split(" ")[1])
-
-unimportantVars = varCount - levelCount #amount of flags that can be turned on or off no matter what
-
-sats = [0 for i in nodes]
-varMap = [int(i)+1 for i in raw[7].split(" ")[1:]]
-vars = [0 for i in nodes]
-tree = [() for i in nodes]
-levels = [levelCount for i in nodes]
-
-for n in range(len(nodes)):
-	node = nodes[n].split(" ")
-	if node[1] == "F": #false has no solutions
-		pass #LEVEL[n] = node[1]
-
-	elif node[1] == "T": #true has one solution
-		sats[n] = 1
-
-	else:	#otherwise, sum solution count of both children
-		a, b = int(node[2])-1, int(node[3])-1
-		l = int(node[1])
-		levels[n] = l
-
-		#difference in level implies levels are skipped and can be arbitrarily assigned
-		sats[n] = sats[a] * 2 ** (levels[a] - l - 1) + sats[b] * 2 ** (levels[b] - l - 1)
-
-		if sats[n] != int(sats[n]):
-			print("FUCK")
-			print(n)
-			print(sats[n])
-
-		sats[n] = int(sats[n])
-
-		vars[n] = varMap[int(node[1])]
-		tree[n] = (a, sats[a], b, sats[b])
+from math import log
 
 
+class BDD:
+	def __init__(self, filename):
+		print(f"PARSING {filename}")
+		with open(f"dddmps/{filename}.dddmp") as file:
+			raw = file.read().split("\n")
+
+		self.nodes = raw[11:-2]
+
+		levelCount = int(raw[5].split(" ")[1])
+
+		self.varCount = int(raw[4].split(" ")[1])
+
+		unimportantVars = self.varCount - levelCount #amount of flags that can be turned on or off no matter what
 
 
-s = sats[-1] * 2 ** unimportantVars
-print(f"total sats: {s} ({s:.3E})")
+		self.sats = [0 for _ in self.nodes]
+		varMap = [int(i)+1 for i in raw[7].split(" ")[1:]]
+		self.vars = [0 for _ in self.nodes]
+		self.tree = [() for _ in self.nodes]
+		self.levels = [levelCount for _ in self.nodes]
+
+		for n in range(len(self.nodes)):
+			node = self.nodes[n].split(" ")
+			if node[1] == "F": #false has no solutions
+				pass
+
+			elif node[1] == "T": #true has one solution
+				self.sats[n] = 1
+
+			else:	#otherwise, sum solution count of both children
+				a, b = int(node[2])-1, int(node[3])-1
+				l = int(node[1])
+				self.levels[n] = l
+
+				#difference in level implies levels are skipped and can be arbitrarily assigned
+				self.sats[n] = self.sats[a] * 2 ** (self.levels[a] - l - 1) + self.sats[b] * 2 ** (self.levels[b] - l - 1)
+
+				self.sats[n] = int(self.sats[n])
+
+				self.vars[n] = varMap[self.levels[n]]
+				self.tree[n] = (a, self.sats[a], b, self.sats[b])
+
+
+
+
+		s = self.sats[-1] * 2 ** unimportantVars
+#		print(f"total sats: {s} ({s:.3E})")
+		print(f"total sats: {s} (E+{int(log(s,10))})")
+
+		K = 10_000
+		r = self.ratio(42, K)
+		x = 42
+
+		print(f"x{x} selected {r} / 10_000 random samples (k1/k0 = {r / (K-r):.3F})")
+
+
+	def sample(self):
+		s = [None for i in range(self.varCount)]
+		i = len(self.nodes) - 1
+		while i > 1:
+			t = self.tree[i]
+			x = rng(t[1], t[3])
+			s[self.vars[i]-1] = x
+			i = t[0] if x else t[2]
+
+		#fill vars that dont matter with 50/50
+		for i in range(len(s)):
+			if s[i] == None: s[i] = rng(1, 1)
+
+		return s
+
+	def ratio(self, i, k):
+		return sum([self.sample()[i] for _ in range(k)])
+
+
 
 def rng(a, b):
 	return randrange(a+b) < a
 
 
-def sample():
-	s = [None for i in range(varCount)]
-	i = len(nodes) - 1
-	while i > 1:
-		t = tree[i]
-		x = rng(t[1], t[3])
-		s[vars[i]-1] = x
-		i = t[0] if x else t[2]
-
-	#fill vars that dont matter with 50/50
-	for i in range(len(s)):
-		if s[i] == None: s[i] = rng(1, 1)
-
-	return s
 
 
-def ratio(i, k):
-	tmp = [sample()[i] for _ in range(k)]
-	return sum(tmp)
 
 
-print(f"x42 selected {ratio(41, 10_000)} / 10_000 times")
+
+for i in ["buildroot", "busybox", "embtoolkit", "toybox", "uClinux"]:
+        BDD(i)
+#for i in ["toybox", "uClinux"]:
+ #       BDD(i)
--- 5-CST.py
+++ 5-CST.py
@@ -2,49 +2,95 @@
 from oxidd.bdd import BDDManager
 
 
-with open("conf-dimacs/toybox.dimacs") as file:
-        raw = file.read().split("\n")
 
-n = int(raw[0].split(" ")[2])
-m = int(raw[0].split(" ")[3])
+class BDD:
+	def __init__(self, filename):
+		print(f"PARSING {filename}")
+		with open(f"conf-dimacs/{filename}.dimacs") as file:
+		        raw = file.read().split("\n")
 
-ovars = [int(i)-1 for i in raw[1].split(" ")[2:]]
+		self.varCount = int(raw[0].split(" ")[2])
+		self.clauseCount = int(raw[0].split(" ")[3])
+
+		self.varOrder = [int(i)-1 for i in raw[1].split(" ")[2:]]
+		self.manager = BDDManager(1_000_000_000, 100_000_000, 12)
+		self.vars = [self.manager.var(i) for i in self.manager.add_vars(self.varCount)]
+		self.manager.set_var_order(self.varOrder)
+
+		print(f"{self.varCount} Variables | {self.clauseCount} Clauses")
+
+		a = []
+		clauses = [[int(j) for j in i.split(" ")[:-1]] for i in raw[2:-1]]
+
+		for v in clauses:
+			a.append(self.vars[v[0] - 1] if v[0] > 0 else ~self.vars[-v[0]-1])
+			for j in v[1:]:
+				if j > 0:
+					a[-1] |= self.vars[j-1]
+				else:
+					a[-1] |= ~self.vars[-j-1]
+
+		print("Clauses parsed")
 
 
-manager = BDDManager(1_000_000_000, 100_000_000, 12)
-vars = [manager.var(i) for i in manager.add_vars(n)]
-manager.set_var_order(ovars)
+		self.bdd = a[0]
+		for i in a[1:]:
+			self.bdd &= i
 
-print(manager.num_vars())
-
-#vars.reverse()
-
-print("VARS MADE")
-
-a = []
-for i in raw[2:-1]:
-	v = [int(j) for j in i.split(" ")[:-1]]
-	a.append(vars[v[0] - 1] if v[0] > 0 else ~vars[-v[0]-1])
-	for j in v[1:]:
-		if j > 0:
-			a[-1] |= vars[j-1]
+		print("Built full BDD")
+		if self.bdd.satisfiable():
+			print("Satisfiability verified")
 		else:
-			a[-1] |= vars[-j-1]
+			print("BDD IS NOT SATISFIABLE!!!!")
+		print(f"{self.bdd.node_count()} Nodes")
 
-print("MADE CLAUSES")
+#
+#		self.pairs = set()
+#
+#		for i in range(self.varCount):
+#			for j in range(i):
+#				if (self.bdd & self.vars[i] & self.vars[j]).satisfiable():
+#					self.pairs.add((i, True, j, True))
+#				if (self.bdd & self.vars[i] & ~self.vars[j]).satisfiable():
+#					self.pairs.add((i, True, j, False))
+#				if (self.bdd & ~self.vars[i] & self.vars[j]).satisfiable():
+#					self.pairs.add((i, False, j, True))
+#				if (self.bdd & ~self.vars[i] & ~self.vars[j]).satisfiable():
+#					self.pairs.add((i, False, j, False))
+#			if i % 100 == 0:
+#				self.manager.gc()
+#
+#		print(f"Found {len(self.pairs)} Interaction pairs")
+#
+#
+#		self.cover = []
+#
+#		#greedy cover algorithm
+#		for p in self.pairs:
+#			covered = False
+#			fits = None
+#			for c in self.cover:
+#				if c[p[0]] == p[1] and c[p[2]] == p[3]: #already covered
+#					covered = True
+#					break
+#				elif (c[p[0]] == p[1] or c[p[0]] == None) and (c[p[2]] == p[3] or c[p[3]] == None): #could be covered here
+#					fits = c
+#
+#			if not covered:
+#				if fits == None:
+#					fits = [None for _ in range(self.varCount)]
+#					self.cover.append(fits)
+#
+#				fits[p[0]] = p[1]
+#				fits[p[2]] = p[3]
+#
+#		print(f"Created cover of size {len(self.cover)}")
 
-b = a[0]
-c = 0
-for i in a[1:]:
-	b &= i
-	c += 1
+
+		self.manager.export_dddmp(f"dddmps/{filename}.dddmp", [self.bdd])
+		print("Exported dddmp file, use 5-CST-URS.py for more data")
 
 
-print("BUILT")
 
-
-print(b.satisfiable())
-print(b.node_count())
-
-manager.export_dddmp("tmp.dddmp", [b])
-print("EXPORTED, use 5-CST-URS.py for more data")
+for i in ["buildroot", "busybox", "embtoolkit", "toybox", "uClinux"]:
+	BDD(i)
